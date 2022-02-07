3a72aebc596319ca2c9a699a4ca3a2b2584983f3
Kunal Bhatti <kunal.bhatti@gmail.com>
2022-02-07 17:39:08 +0530

    reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learning_angular';
+
+  name!: string;
+  age!: number;
+
+  reactiveForm = new FormGroup({
+    name: new FormControl(''),
+    age: new FormControl('')
+  })
+
+  onSubmit(): void {
+    let value: {
+      name: string,
+      age: number
+    } = this.reactiveForm.value;
+
+    this.name = value.name;
+    this.age = value.age;
+
+  }
+
 }
