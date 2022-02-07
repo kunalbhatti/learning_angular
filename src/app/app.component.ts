c99163d76464b75cf8dbd06b6f7cd21473ae6ed8
Kunal Bhatti <kunal.bhatti@gmail.com>
2022-02-07 20:21:30 +0530

    user form | table | edit functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,42 +2,113 @@ import {
   Component
 } from '@angular/core';
 import {
+  FormArray,
   FormControl,
   FormGroup,
   Validators
 } from '@angular/forms';
+import {
+  User
+} from './user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 
-  name!: string;
-  age!: number;
-  gender!: string;
+  users: User[] = [];
 
-  submitted: boolean = false;
+  updateId: number = -1;
 
-  reactiveForm = new FormGroup({
+  foodTypes: string[] = ['pizza', 'burger', 'noodles'];
+
+  //favFood checkbox validator not working
+
+  userForm: FormGroup = new FormGroup({
     name: new FormControl('', [Validators.required]),
-    age: new FormControl('', [Validators.required]),
-    gender: new FormControl('', [Validators.required])
+    email: new FormControl('', [Validators.required, Validators.email]),
+    country: new FormControl('', [Validators.required]),
+    state: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    favFood: new FormArray([
+      new FormGroup({
+        pizza: new FormControl(),
+      }), new FormGroup({
+        burger: new FormControl(),
+      }), new FormGroup({
+        noodles: new FormControl(),
+      }),
+    ], [Validators.required]),
+    gender: new FormControl('male'),
+    maritalStatus: new FormControl('single')
   })
 
   onSubmit(): void {
-    this.submitted = true;
-    let value: {
-      name: string,
-      age: number,
-      gender: string
-    } = this.reactiveForm.value;
+    const newUser: User = this.userForm.value;
+    const favFoodArr: string[] = [];
 
-    this.name = value.name;
-    this.age = value.age;
-    this.gender = value.gender;
+    for(let i in  newUser.favFood){
+      if(newUser.favFood[i][this.foodTypes[i]]){        
+        favFoodArr.push(this.foodTypes[i]);
+      }
+    }
 
+    newUser.favFood = favFoodArr;
+
+    this.users.push(newUser);
+    this.userForm.reset();
+    this.userForm.get('maritalStatus')?.setValue('default');
+  }
+
+  onEdit(id: number): void{
+    this.updateId = id;
+    const userData: User = this.users[id];
+
+    this.userForm.get('name')?.setValue(userData.name);
+    this.userForm.get('email')?.setValue(userData.email);
+    this.userForm.get('country')?.setValue(userData.country);
+    this.userForm.get('state')?.setValue(userData.state);
+    this.userForm.get('city')?.setValue(userData.city);
+    this.userForm.get('gender')?.setValue(userData.gender);
+    this.userForm.get('maritalStatus')?.setValue(userData.maritalStatus);
+
+    //Update checkbox: how to set the value of the form array from code
+    // const favFood: string[] = userData.favFood;
+     
+    // for(let fav of favFood){
+    //   const index: number = this.foodTypes.findIndex(food => {
+    //     return food === fav;
+    //   });
+
+    //   this.userForm.get('favFood')?.get(index.toString())?.setValue(0);
+      
+    // }
+   
+  }
+
+  onDelete(id: number): void{
+    this.users.splice(id, 1)
+  }
+
+  onUpdate(): void{
+    const userUpdate: User = this.userForm.value;
+
+    const favFoodArr: string[] = [];
+    for(let i in  userUpdate.favFood){
+      if(userUpdate.favFood[i][this.foodTypes[i]]){        
+        favFoodArr.push(this.foodTypes[i]);
+      }
+    }
+
+    userUpdate.favFood = favFoodArr;
+
+    this.users[this.updateId] = userUpdate;
+    this.userForm.reset();
+
+    this.updateId = -1;
   }
 
 }
---
+++ src/app/user.model.ts
@@ -0,0 +1,10 @@
+export interface User {
+  name: string;
+  email: string;
+  country: string;
+  state: string;
+  city: string;
+  favFood: any[];
+  gender: string;
+  maritalStatus: string;
+}
