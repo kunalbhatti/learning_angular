0320fb3e3e21fa87cf3459a2a319183f01a516c5
Kunal Bhatti <kunal.bhatti@gmail.com>
2022-02-07 18:02:32 +0530

    validation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import {
 } from '@angular/core';
 import {
   FormControl,
-  FormGroup
+  FormGroup,
+  Validators
 } from '@angular/forms';
 
 @Component({
@@ -15,20 +16,27 @@ export class AppComponent {
 
   name!: string;
   age!: number;
+  gender!: string;
+
+  submitted: boolean = false;
 
   reactiveForm = new FormGroup({
-    name: new FormControl(''),
-    age: new FormControl('')
+    name: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required]),
+    gender: new FormControl('', [Validators.required])
   })
 
   onSubmit(): void {
+    this.submitted = true;
     let value: {
       name: string,
-      age: number
+      age: number,
+      gender: string
     } = this.reactiveForm.value;
 
     this.name = value.name;
     this.age = value.age;
+    this.gender = value.gender;
 
   }
 
